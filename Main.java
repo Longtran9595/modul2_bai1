8d005d62613b3e8d0f89bff605e05390fcc767de
Longtran9595 <longktqd95@gmail.com>
2023-09-07 16:20:39 +0700

    lesson 1

---
+++ Main.java
@@ -0,0 +1,203 @@
+import java.util.Scanner;
+
+public class Main {
+//    Nhập xuất:
+//    bai1
+//    public static void main(String[] args) {
+//        String a = "hello world";
+//        System.out.println(a);
+//        }
+
+
+//    bài2
+//    public static void main(String[] args) {
+//        String b = "Hello world, Hi Ae";
+//        System.out.println(b);
+//    }
+
+
+//    bài3
+//public static void main(String[] args) {
+//    Scanner a = new Scanner(System.in);
+//
+//    System.out.print("Nhập tên của bạn:");
+//    String ten = a.nextLine();
+//
+//    System.out.println("Hello "+ten);
+//
+//}
+
+
+//    Kiểu dữ liệu:
+//    bài1
+//public static void main(String[] args) {
+//    int a = 6;
+//    System.out.println(a);
+//}
+
+//    bài2
+//public static void main(String[] args) {
+//    Scanner scanner = new Scanner(System.in);
+//    System.out.print("Nhập 1 số ");
+//    int SoInt = scanner.nextInt();
+//    System.out.println("số b nhập là "+SoInt);
+//}
+
+//    bài3
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Nhập 2 số: ");
+//        int soA = scanner.nextInt();
+//        int soB = scanner.nextInt();
+//
+//        int tong = soA + soB;
+//        int tich = soA * soB;
+//        int hieu = soA - soB;
+//        int thuong = soA / soB;
+//        int chiaDu = soA % soB;
+//
+//        System.out.println("Tổng là " + tong);
+//        System.out.println("Tích là " + tich);
+//        System.out.println("Hiệu là " + hieu);
+//        System.out.println("Thương là " + thuong);
+//        System.out.println("Chia lấy dư là " + chiaDu);
+//    }
+
+
+//    bài 4
+//public static void main(String[] args) {
+//    Scanner scanner = new Scanner(System.in);
+//
+//    System.out.println("Nhập tên, tuổi ");
+//    String ten = scanner.nextLine();
+//    int tuoi = scanner.nextInt();
+//
+//    System.out.println("xin chào tên tôi là " + ten + " năm nay tôi "+ tuoi + "tuổi");
+//}
+
+
+//    Toán tử:
+//    bài1
+//public static void main(String[] args) {
+//    Scanner input = new Scanner(System.in);
+//    System.out.println("Enter 2 numbers ");
+//
+//    int a = input.nextInt();
+//    int b = input.nextInt();
+//
+//    System.out.println("Result: ");
+//    System.out.println(a+b);
+//    System.out.println(a-b);
+//    System.out.println(a*b);
+//}
+
+//        public static void main(String[] args) {
+//            int a = 5;
+//            int b = 10;
+//
+//            // Less than (<) operator
+//            boolean lessThan = a < b; // Is 'a' less than 'b'?
+//            System.out.println("a < b: " + lessThan); // true
+//
+//            // Greater than (>) operator
+//            boolean greaterThan = a > b; // Is 'a' greater than 'b'?
+//            System.out.println("a > b: " + greaterThan); // false
+//
+//            // Less than or equal to (<=) operator
+//            boolean lessThanOrEqual = a <= b; // Is 'a' less than or equal to 'b'?
+//            System.out.println("a <= b: " + lessThanOrEqual); // true
+//
+//            // Greater than or equal to (>=) operator
+//            boolean greaterThanOrEqual = a >= b; // Is 'a' greater than or equal to 'b'?
+//            System.out.println("a >= b: " + greaterThanOrEqual); // false
+//
+//            // Equality (==) operator
+//            boolean isEqual = a == b; // Are 'a' and 'b' equal?
+//            System.out.println("a == b: " + isEqual); // false
+//
+//            // Inequality (!=) operator
+//            boolean notEqual = a != b; // Are 'a' and 'b' not equal?
+//            System.out.println("a != b: " + notEqual); // true
+//        }
+
+
+        //   Điều kiện
+//    bài 1
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("nhập số a: ");
+//        int a = input.nextInt();
+//        System.out.println("nhập b: ");
+//        int b = input.nextInt();
+//
+//        int c = a % b;
+//        System.out.println("kết quả");
+//
+//        if ( c == 0 ) {
+//            System.out.println("a có chia hết cho b");
+//        } else {
+//            System.out.println("a không chia hết cho b");
+//        }
+//    }
+
+//    bài 2
+//public static void main(String[] args) {
+//    Scanner tuoi = new Scanner(System.in);
+//    System.out.println("nhập tuổi:");
+//    int a = tuoi.nextInt();
+//
+//    System.out.println("ket qua");
+//    if (a ==  15) {
+//        System.out.println("đủ dieu kien vao 10");
+//    } else {
+//        System.out.println("khong du dkien vao 10");
+//    }
+//
+//}
+
+//    bài 3
+//public static void main(String[] args) {
+//    Scanner input = new Scanner(System.in);
+//    System.out.println("nhập số");
+//    int a = input.nextInt();
+//    System.out.println("check");
+//    if (a < 0) {
+//        System.out.println("nhỏ hơn 0");
+//    } else if (a == 0) {
+//        System.out.println("bằng 0");
+//    } else {
+//        System.out.println("lớn hơn 0");
+//    }
+//}
+
+//        bài 4
+//public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("input the first number: ");
+//        int a = input.nextInt();
+//        System.out.println("input the second number: ");
+//        int b = input.nextInt();
+//        System.out.println("input the thirst number: ");
+//        int c = input.nextInt();
+//
+//        System.out.println("the max number:");
+//        if (a >= b && a >= c) {
+//            System.out.println(a);
+//        } else if (b >= c && b >= a) {
+//            System.out.println(b);
+//        } else {
+//            System.out.println(c);
+//        }
+//    }
+
+//  bai 5
+public static void main(String[] args) {
+
+}
+
+}
+
+
+
